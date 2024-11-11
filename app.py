af3a2b8991231c263e230d92a7eb64ac2c6bcd5e
Cristian Alemán <bladimiralemanc@gmail.com>
2024-11-11 01:29:54 -0500

    feat: :sparkles: Added new function "protected" to test the JWT access token

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+#Imoort classes and functions needed to this service
 from flask import Flask, jsonify, request
-from flask_jwt_extended import JWTManager, create_access_token
+from flask_jwt_extended import JWTManager,jwt_required, create_access_token, get_jwt_identity 
 
+#Create an instance of Flask and a JWT secret key for the tokens and initialize JWTManager
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = "cbaleman04" 
 jwt = JWTManager(app)
 
+#The route that we're using to use the method login
 @app.route('/login', methods=['POST'])
 def login():
     username = request.json.get("username", None)
@@ -14,6 +17,14 @@ def login():
 
     access_token = create_access_token(identity=username)
     return jsonify(access_token=access_token)
+
+# protected route
+@app.route('/protected', methods=['GET'])
+@jwt_required()  # This requires the secret token
+def protected():
+    # If the token is right we've get the identity of the user
+    current_user = get_jwt_identity()  # get the jwt identity
+    return jsonify(logged_in_as=current_user), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
